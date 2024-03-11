885815a32794539d7d747488bc2ceeaaa3cce440
Aishwarya Shinde <107830080+admin1712@users.noreply.github.com>
2024-03-11 10:46:01 +0530

    Update Footer.jsx

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -30,8 +30,15 @@ const Footer = () => {
                         <i className="uil uil-twitter"></i>
                     </a>
 
-                    <a href="" className="footer__social" target="_blank">
-                        <span className="uim-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em"><g data-name="Brand Logos"><path className="uim-primary" d="M5.00351,7H4.97535a2.23465,2.23465,0,1,1,.05658-4.45706A2.23549,2.23549,0,1,1,5.00351,7Z"></path><rect width="4" height="12" x="3.018" y="10" className="uim-primary"></rect><path className="uim-primary" d="M17.51754,10a4.47251,4.47251,0,0,0-3.5,1.70343V10h-4V22h4V16.5a2,2,0,0,1,4,0V22h4V14.5A4.5,4.5,0,0,0,17.51754,10Z"></path></g></svg></span>
+                    <a href="https://www.linkedin.com/in/Aishwarya-Shinde17/" className="footer__social" target="_blank">
+                        <span className="uim-svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em">
+                          <g data-name="Brand Logos">
+                            <path className="uim-primary" d="M5.00351,7H4.97535a2.23465,2.23465,0,1,1,.05658-4.45706A2.23549,2.23549,0,1,1,5.00351,7Z">
+                          </path>
+                            <rect width="4" height="12" x="3.018" y="10" className="uim-primary">
+                            </rect>
+                            <path className="uim-primary" d="M17.51754,10a4.47251,4.47251,0,0,0-3.5,1.70343V10h-4V22h4V16.5a2,2,0,0,1,4,0V22h4V14.5A4.5,4.5,0,0,0,17.51754,10Z">
+                            </path></g></svg></span>
                     </a>
                 </div>
             </div>
